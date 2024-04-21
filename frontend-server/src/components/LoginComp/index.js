5155ea8235f10ce493e2459881908dcc9006f3c0
arpit5492 <arpitmallick99@gmail.com>
2024-04-20 23:28:34 -0700

    SPA route implementing

--- frontend-server/src/components/LoginComp/index.js
+++ frontend-server/src/components/LoginComp/index.js
@@ -1,10 +1,18 @@
 import { useEffect, useState } from "react";
 
 function Login() {
-  const [loginData, setData] = useState({
+  const initial = () => {
+    return {
+      username: "",
+      password: "",
+      checked: false
+    }
+  }
+  const [loginData, setData] = useState(() => initial());
+
+  const [head, setHead] = useState({
     username: "",
-    password: "",
-    checked: false
+    password: ""
   });
 
   useEffect(() => {
@@ -18,17 +26,18 @@ function Login() {
         ...prevState, [e.target.name]: e.target.type === "checkbox" ? e.target.checked : e.target.value
       }
     });
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setHead({username: loginData.username, password: loginData.password});
   }
 
   return (
     <div>
       <form action="" onSubmit={handleSubmit} style={{border: "1px solid black", height: "50vh"}}>
         <div>
-          <h5>{JSON.stringify(loginData)}</h5>
+          <h5>{head.username} {head.password}</h5>
         </div>
         <div style={{marginTop: "10%"}}>
           <label>
@@ -36,14 +45,12 @@ function Login() {
           </label>
           <input type="text" onChange={handleChange} name="username"/>
         </div>
-        {loginData.checked && 
-          <div>
-            <label>
-              Password
-            </label>
-            <input type="password" onChange={handleChange} name="password"/>
-          </div>
-        }
+        <div>
+          <label>
+            Password
+          </label>
+          <input type="password" onChange={handleChange} name="password"/>
+        </div>
         <div>
           Click Here <input type="checkbox" onChange={handleChange} name="checked" />
         </div>
