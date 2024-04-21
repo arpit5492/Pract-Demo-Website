cba209790f1e523b8b1bed16b8aabce5412f2b7b
arpit5492 <arpitmallick99@gmail.com>
2024-04-20 23:03:56 -0700

    Conditional rendering implemented

--- frontend-server/src/components/LoginComp/index.js
+++ frontend-server/src/components/LoginComp/index.js
@@ -1,15 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Login() {
   const [loginData, setData] = useState({
     username: "",
-    password: ""
+    password: "",
+    checked: false
   });
 
+  useEffect(() => {
+    console.log(loginData);
+  }, [loginData]);
+
   const handleChange = (e) => {
+    // console.log(e.target.type);
     setData(prevState => {
       return {
-        ...prevState, [e.target.name]: e.target.value
+        ...prevState, [e.target.name]: e.target.type === "checkbox" ? e.target.checked : e.target.value
       }
     });
   }
@@ -21,24 +27,32 @@ function Login() {
   return (
     <div>
       <form action="" onSubmit={handleSubmit} style={{border: "1px solid black", height: "50vh"}}>
+        <div>
+          <h5>{JSON.stringify(loginData)}</h5>
+        </div>
         <div style={{marginTop: "10%"}}>
-          <div>
-            <h5>{JSON.stringify(loginData)}</h5>
-          </div>
           <label>
             Username
           </label>
           <input type="text" onChange={handleChange} name="username"/>
         </div>
+        {loginData.checked && 
+          <div>
+            <label>
+              Password
+            </label>
+            <input type="password" onChange={handleChange} name="password"/>
+          </div>
+        }
         <div>
-          <label>
-            Password
-          </label>
-          <input type="password" onChange={handleChange} name="password"/>
+          Click Here <input type="checkbox" onChange={handleChange} name="checked" />
         </div>
         <div>
-          <button>
+          <button style={{marginRight: "4px", marginTop: "4px"}}>
             Sign In
+          </button>
+          <button>
+            Sign Up
           </button>
         </div>
       </form>
