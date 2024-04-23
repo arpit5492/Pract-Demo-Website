5c574f5bb85d974a1e9c1450e5965284d4feb349
arpit5492 <arpitmallick99@gmail.com>
2024-04-23 15:20:52 -0700

    Fetch api working fine and data is retrieving

--- frontend-server/src/components/HomeComp/index.js
+++ frontend-server/src/components/HomeComp/index.js
@@ -1,7 +1,47 @@
+import { useEffect, useState } from "react";
+import "./_home-comp.scss";
+
 function HomeComp() {
+
+  const [data, setData] = useState([]);
+  const [errFlag, setErrFlag] = useState(false);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/getProd")
+      .then(res => {
+        if(res.status === 200) {
+          return res.json();
+        } else {
+          throw new Error("Error!!");
+        }
+      })
+      .then(json => {
+        setData(json);
+      })
+      .catch((err) => {
+        setErrFlag(true);
+      })
+      console.log(data);
+  }, []);
+  
   return (
-    <div>
-      <h2>Home Componenent</h2>
+    <div className="prod-comp">
+      {JSON.stringify(data)}
+      {data.map(item => {
+        return (
+          <div className="prod-card">
+            <div>
+              <img src={item.prod_img} alt={item.pName} />
+            </div>
+            <div>
+              <h4>{item.pName}</h4>
+            </div>
+            <div className="price">
+              <p><span>Price: </span>${item.price}</p>
+            </div>
+          </div>
+        )
+      })}
     </div>
   )
 }
